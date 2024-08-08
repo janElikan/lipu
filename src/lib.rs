e68def60688d2a8481db5251c9e4dec26abf9975
jan Elikan <janelikan@proton.me>
2024-08-08 08:10:30 +0000

    refactor of the refactor

--- src/lib.rs
+++ src/lib.rs
@@ -25,33 +25,32 @@ impl Display for Article {
 
 #[derive(Debug)]
 enum Error {
-    NetworkError,
-    FetchTextFailed,
-    FeedParsingFailed,
+    Network,
+    ResponseProcessing,
+    FeedParsing,
 }
 
 async fn fetch(feed_url: &str) -> Result<Vec<Article>, Error> {
     let xml = reqwest::get(feed_url)
         .await
-        .map_err(|_| Error::NetworkError)?
+        .map_err(|_| Error::Network)?
         .text()
         .await
-        .map_err(|_| Error::FetchTextFailed)?;
+        .map_err(|_| Error::ResponseProcessing)?;
 
-    let feed = feed_rs::parser::parse(xml.as_bytes()).map_err(|_| Error::FeedParsingFailed)?;
+    let feed = feed_rs::parser::parse(xml.as_bytes()).map_err(|_| Error::FeedParsing)?;
 
     Ok(feed
         .entries
         .into_iter()
         .map(Article::try_from)
-        .map(|result| match result {
+        .flat_map(|result| match result {
             Ok(data) => Ok(data),
             Err(why) => {
-                println!("download item failed: `{:?}`", why);
+                println!("download item failed: `{why:?}`");
                 Err(why)
             }
         })
-        .flatten()
         .collect())
 }
 
@@ -84,6 +83,7 @@ impl App {
         self
     }
 
+    /// Turns the `App` into a `Vec`tor of `Article`s, sorted by their publication date
     pub async fn fetch(&self) -> Vec<Article> {
         let mut feeds = Vec::new();
 
@@ -94,14 +94,13 @@ impl App {
 
         let mut feeds: Vec<Article> = feeds
             .into_iter()
-            .map(|result| match result {
+            .flat_map(|result| match result {
                 Ok(data) => Ok(data),
                 Err(why) => {
-                    println!("download feed failed: `{:?}`", why);
+                    println!("download feed failed: `{why:?}`");
                     Err(why)
                 }
             })
-            .flatten()
             .flatten()
             .map(|article| {
                 if let Some(others) = &self.merge_with {
@@ -120,7 +119,11 @@ impl App {
             })
             .collect();
 
-        feeds.sort_by(|a, b| b.created.partial_cmp(&a.created).expect("sorting failed"));
+        feeds.sort_by(|a, b| {
+            b.created
+                .partial_cmp(&a.created)
+                .map_or(std::cmp::Ordering::Less, |ord| ord)
+        });
 
         feeds
     }
