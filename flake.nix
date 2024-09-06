32ff78f412ce02cb7aa42bc1e2672df5542d4921
jan Elikan <janelikan@proton.me>
2024-09-06 13:30:52 +0000

    build: update rust

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     naersk.url = "github:nix-community/naersk/master";
     fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
   };
@@ -17,20 +18,31 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
 
-        naersk-lib = pkgs.callPackage naersk {};
+        toolchain = with fenix.packages.${system};
+          combine [
+            complete.rustc
+            complete.rustfmt
+            complete.cargo
+            complete.clippy
+            targets.wasm32-unknown-unknown.latest.rust-std
+          ];
       in {
-        defaultPackage = naersk-lib.buildPackage {
-          src = ./.;
-        };
+        defaultPackage =
+          (naersk.lib.${system}.override {
+            cargo = toolchain;
+            rustc = toolchain;
+          })
+          .buildPackage {
+            src = ./.;
+            CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+          };
         devShell = with pkgs;
           mkShell {
             buildInputs = [
-              cargo
-              rustc
-              rustfmt
-              rustPackages.clippy
-              bacon
+              toolchain
               rust-analyzer
+              bacon
+              trunk
               (pkgs.writeShellScriptBin "run" "npm run tauri dev")
             ];
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
